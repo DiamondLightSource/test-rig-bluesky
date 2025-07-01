d92bf88a2c55f4cadffb8efdee0590c0d6ce083f
Callum Forrester <callum.forrester@diamond.ac.uk>
2025-07-01 15:34:29 +0000

    Create system test that counts a detector

---
+++ tests/system_tests/test_take_data.py
@@ -0,0 +1,45 @@
+from concurrent.futures import Future
+from pathlib import Path
+from typing import Any
+
+from blueapi.client.client import BlueapiClient
+from blueapi.worker.task import Task
+from bluesky_stomp.messaging import MessageContext, StompClient
+from bluesky_stomp.models import MessageTopic
+
+
+def test_collect_data(
+    client: BlueapiClient,
+    stomp_client: StompClient,
+    latest_comissioning_instrument_session: str,
+    data_directory: Path,
+) -> None:
+    # Listen for NeXus file events, see below
+    nexus_finished_message = Future()
+
+    def on_nexus_message(message: dict[str, Any], _: MessageContext) -> None:
+        if message["status"] == "FINISHED":
+            nexus_finished_message.set_result(message)
+
+    stomp_client.subscribe(MessageTopic(name="gda.messages.scan"), on_nexus_message)
+
+    # Run plan
+    end_event = client.run_task(
+        Task(
+            name="count",
+            params={"detectors": ["manta"], "num": 5},
+            # instrument_session=latest_comissioning_instrument_session,
+        )
+    )
+    task_id = end_event.task_status.task_id
+
+    # Check task ran and did not error
+    task = client.get_task(task_id)
+    assert task.is_complete
+    assert len(task.errors) == 0
+
+    # Search for a new NeXus file event, with numtracker
+    # we will be able to programatically corelate the
+    # file with the plan, see
+    # https://jira.diamond.ac.uk/browse/DCS-194
+    nexus_finished_message.result(timeout=10.0)
