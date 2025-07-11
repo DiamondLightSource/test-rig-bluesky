2678c3471609d79948862ccc1e618815d41d74d2
Gary Yendell <gary.yendell@diamond.ac.uk>
2025-07-11 10:52:24 +0000

    Update snapshot and spectroscopy system tests to use blueapi

--- tests/system_tests/test_plans_system.py
+++ tests/system_tests/test_plans_system.py
@@ -1,14 +1,6 @@
-from bluesky import RunEngine
-from dodal.beamlines.b01_1 import oav, sample_det, sample_stage
-
-from test_rig_bluesky.plans import snapshot
+def test_snapshot(bluesky_plan_runner):
+    bluesky_plan_runner.run("snapshot")
 
 
-def test_snapshot():
-    RE = RunEngine()
-
-    _sample_det = sample_det(connect_immediately=True)
-    _oav = oav(connect_immediately=True)
-    _sample_stage = sample_stage(connect_immediately=True)
-
-    RE(snapshot(_sample_det, _oav, _sample_stage))
+def test_spectroscopy(bluesky_plan_runner):
+    bluesky_plan_runner.run("spectroscopy")
