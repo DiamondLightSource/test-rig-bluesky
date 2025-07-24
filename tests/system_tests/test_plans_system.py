04774bf3c277770abf199ac1bd85a41af9fe7d02
Gary Yendell <gary.yendell@diamond.ac.uk>
2025-07-24 15:51:58 +0000

    Add system tests using local RunEngine

--- tests/system_tests/test_plans_system.py
+++ tests/system_tests/test_plans_system.py
@@ -1,5 +1,8 @@
 from blueapi.service.model import TaskRequest
+from bluesky import RunEngine
+from dodal.beamlines.b01_1 import imaging_detector, sample_stage, spectroscopy_detector
 
+from test_rig_bluesky.plans import snapshot, spectroscopy
 from test_rig_bluesky.testing import BlueskyPlanRunner
 
 
@@ -17,6 +20,15 @@ def test_snapshot(
     assert events["FINISHED"][0]["scanDimensions"] == [1]
 
 
+def test_snapshot_re():
+    RE = RunEngine()
+    _imaging_detector = imaging_detector(connect_immediately=True)
+    _spectroscopy_detector = spectroscopy_detector(connect_immediately=True)
+    _sample_stage = sample_stage(connect_immediately=True)
+
+    RE(snapshot(_imaging_detector, _spectroscopy_detector, _sample_stage))
+
+
 def test_spectroscopy(
     bluesky_plan_runner: BlueskyPlanRunner, latest_commissioning_instrument_session: str
 ):
@@ -29,3 +41,11 @@ def test_spectroscopy(
         timeout=10,
     )
     assert events["FINISHED"][0]["scanDimensions"] == [5]
+
+
+def test_spectroscopy_re():
+    RE = RunEngine()
+    _spectroscopy_detector = spectroscopy_detector(connect_immediately=True)
+    _sample_stage = sample_stage(connect_immediately=True)
+
+    RE(spectroscopy(_spectroscopy_detector, _sample_stage))
