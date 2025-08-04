13e85acf82fd0c85cceae73dbd9dc96450d77659
Gary Yendell <gary.yendell@diamond.ac.uk>
2025-08-04 13:48:28 +0000

    Update spectroscopy plan to configure ROIs and motion settings

--- tests/system_tests/test_plans_system.py
+++ tests/system_tests/test_plans_system.py
@@ -1,5 +1,9 @@
 from blueapi.service.model import TaskRequest
+from bluesky import RunEngine
+from dodal.beamlines.b01_1 import sample_stage, spectroscopy_detector
+from scanspec.specs import Line
 
+from test_rig_bluesky.plans import spectroscopy
 from test_rig_bluesky.testing import BlueskyPlanRunner
 
 
@@ -29,3 +33,13 @@ def test_spectroscopy(
         timeout=10,
     )
     assert events["FINISHED"][0]["scanDimensions"] == [5]
+
+
+def test_spectroscopy_re():
+    RE = RunEngine()
+    _spectroscopy_detector = spectroscopy_detector(connect_immediately=True)
+    _sample_stage = sample_stage(connect_immediately=True)
+
+    scan_spec = Line(_sample_stage.x, 2, 5, 30) * Line(_sample_stage.y, 0, 5, 50)
+
+    RE(spectroscopy(_spectroscopy_detector, _sample_stage, scan_spec))
