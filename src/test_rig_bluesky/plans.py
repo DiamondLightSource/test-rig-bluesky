dacc95269cafe338663803df67a92b12f349226c
Emily Arnold <222046505+EmsArnold@users.noreply.github.com>
2025-12-08 09:58:53 +0000

    Update spectroscopy detector whitelist (#37)
    * add pvs to settings whitelist for loading

--- src/test_rig_bluesky/plans.py
+++ src/test_rig_bluesky/plans.py
@@ -86,6 +86,10 @@ def spectroscopy(
         design_name="spectroscopy_detector_baseline",
         whitelist_pvs=[
             "fileio-nd_array_port",
+            "fileio-enable_callbacks",
+            "driver-acquire",
+            "driver-trigger_mode",
+            "driver-trigger_source",
             "roistat-channels-array_counter",
             "roistat-channels-1-min_x",
             "roistat-channels-1-min_y",
@@ -105,6 +109,8 @@ def spectroscopy(
             "roistat-channels-3-size_x",
             "roistat-channels-3-size_y",
             "roistat-channels-3-use",
+            "roistat-nd_array_port",
+            "roistat-enable_callbacks",
         ],
     )
 
