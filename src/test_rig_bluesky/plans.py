4de74fd07a75bc54935fcec0f2badf85e1a4aed4
Shihab Suliman <shihab.suliman@diamond.ac.uk>
2026-01-30 11:11:38 +0000

    refactor: use injected pmac

--- src/test_rig_bluesky/plans.py
+++ src/test_rig_bluesky/plans.py
@@ -35,7 +35,6 @@ from ophyd_async.fastcs.panda import (
 from ophyd_async.plan_stubs import (
     apply_settings,
     apply_settings_if_different,
-    ensure_connected,
     retrieve_settings,
     setup_ndattributes,
     store_settings,
@@ -182,7 +181,7 @@ def spectroscopy(
 def spectroscopy_fly(
     spectroscopy_detector: AravisDetector = spectroscopy_detector,
     sample_stage: XYZStage = sample_stage,
-    # pmac: PmacIO = pmac,
+    pmac: PmacIO = pmac,
     pandabrick: HDFPanda = pandabrick,
     spec: Spec[Movable] | None = None,
     exposure_time: float = 0.1,
@@ -239,14 +238,6 @@ def spectroscopy_fly(
         )
 
     yield from setup_ndattributes(spectroscopy_detector.roistat, params)  # type: ignore
-
-    pmac = PmacIO(
-        "BL01C-MO-PPMAC-01:",
-        raw_motors=[sample_stage.y, sample_stage.x],
-        coord_nums=[1],
-    )
-
-    yield from ensure_connected(pmac)
 
     # Prepare motor info using trajectory scanning
     scan_frame_duration = 0.01
@@ -348,7 +339,7 @@ def spectroscopy_fly(
 def demo_spectroscopy(
     spectroscopy_detector: AravisDetector = spectroscopy_detector,
     sample_stage: XYZStage = sample_stage,
-    # pmac: PmacIO = pmac,
+    pmac: PmacIO = pmac,
     pandabrick: HDFPanda = pandabrick,
     total_number_of_scan_points: int = 25,
     grid_size: float = 5.0,
@@ -361,15 +352,15 @@ def demo_spectroscopy(
     The time taken to scan is approximately linear in total_numbers_of_grid_points.
     All other parameters can be left at their defaults.
     """
+    xsteps = ysteps = int(round(math.sqrt(max(total_number_of_scan_points, 1))))
+    xmin = grid_origin_x
+    xmax = grid_origin_x + grid_size
+    ymin = grid_origin_y
+    ymax = grid_origin_y + grid_size
+    grid = Line(sample_stage.y, ymin, ymax, ysteps) * Line(
+        sample_stage.x, xmin, xmax, xsteps
+    )
     if total_number_of_scan_points < 1000:
-        xsteps = ysteps = int(round(math.sqrt(max(total_number_of_scan_points, 1))))
-        xmin = grid_origin_x
-        xmax = grid_origin_x + grid_size
-        ymin = grid_origin_y
-        ymax = grid_origin_y + grid_size
-        grid = Line(sample_stage.y, ymin, ymax, ysteps) * Line(
-            sample_stage.x, xmin, xmax, xsteps
-        )
         yield from spectroscopy(
             spectroscopy_detector=spectroscopy_detector,
             sample_stage=sample_stage,
@@ -381,7 +372,7 @@ def demo_spectroscopy(
         yield from spectroscopy_fly(
             spectroscopy_detector=spectroscopy_detector,
             sample_stage=sample_stage,
-            # pmac=pmac,
+            pmac=pmac,
             pandabrick=pandabrick,
             spec=None,
             exposure_time=exposure_time,
