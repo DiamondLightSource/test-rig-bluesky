0406387eecd1e2cf63bb09fe3e815ff43abb9997
Shihab Suliman <shihab.suliman@diamond.ac.uk>
2026-02-04 14:31:53 +0000

    refactor: combine scans into common interface

--- src/test_rig_bluesky/plans.py
+++ src/test_rig_bluesky/plans.py
@@ -184,15 +184,21 @@ def spectroscopy(
             metadata=metadata,
         )
     else:
-        yield from _fly_scan(spec, spectroscopy_detector, pandabrick, num_points)  # type: ignore
+        yield from fly_scan(
+            spec,
+            spectroscopy_detector,
+            sample_stage,
+            pandabrick,
+            num_points,  # type: ignore
+        )
 
 
-def _fly_scan(
+def fly_scan(
     spec: Spec[Movable],
-    spectroscopy_detector: AravisDetector,
-    sample_stage: XYZStage,
-    pandabrick: HDFPanda,
-    num_points: int,
+    spectroscopy_detector: AravisDetector = spectroscopy_detector,
+    sample_stage: XYZStage = sample_stage,
+    pandabrick: HDFPanda = pandabrick,
+    num_points: int = 1_000,
 ):
     pmac = PmacIO(
         "BL01C-MO-PPMAC-01:",
